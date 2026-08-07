96dd0f4f0157c8671b747a7afec9fdcd891c3527
Joao-Victor17 <dorea.cupolo@gmail.com>
2026-08-07 18:58:26 -0300

    Fix do mapa temporário

--- app/order/address.tsx
+++ app/order/address.tsx
@@ -1,19 +1,18 @@
-// app/address/index.tsx  (tela do mapa)
 import { useSelectAddress } from "@/hooks/useSelectedAddress";
 import AddressPickerScreen from "@/components/AddressPickerScreen";
 import { useSession } from "@/contexts/ctx";
 
 export default function AddressScreen() {
-	const { handleSelectAddress } = useSelectAddress("newAddress");
-	const { user } = useSession();
+  const { handleSelectAddress } = useSelectAddress("newAddress");
+  const { user } = useSession();
 
-	return (
-		<AddressPickerScreen
-			isLoggedIn={!!user}
-			onAddressConfirmed={handleSelectAddress}
-			onSaveAddress={(address) => {
-				// POST para salvar, depois navega para salvos
-			}}
-		/>
-	);
+  return (
+    <AddressPickerScreen
+    // isLoggedIn={!!user}
+    // onAddressConfirmed={handleSelectAddress}
+    // onSaveAddress={(address) => {
+    // POST para salvar, depois navega para salvos
+    // }}
+    />
+  );
 }
